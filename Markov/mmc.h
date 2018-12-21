29df51e2ca754e34c46ac0c00b25a7dab47a5fe8
Claudio Cicconetti <ccicconetti@mbigroup.it>
2018-12-21 11:46:53 +0100

    Added license stuff.

--- Markov/mmc.h
+++ Markov/mmc.h
@@ -1,3 +1,32 @@
+/*
+ ___ ___ __     __ ____________
+|   |   |  |   |__|__|__   ___/  Ubiquitout Internet @ IIT-CNR
+|   |   |  |  /__/  /  /  /      Tiny Markov simulator
+|   |   |  |/__/  /   /  /       https://bitbucket.org/ccicconetti/markovsim/
+|_______|__|__/__/   /__/
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+Copyright (c) 2018 Claudio Cicconetti <https://about.me/ccicconetti>
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #pragma once
 
 #include "Support/macros.h"
@@ -15,21 +44,17 @@
 namespace uiiit {
 namespace markov {
 
-class MMC final
-{
+class MMC final {
   NONCOPYABLE_NONMOVABLE(MMC);
 
-  using LoadList   = std::list<std::pair<double, unsigned int>>;
+  using LoadList = std::list<std::pair<double, unsigned int>>;
   using DelayArray = std::array<std::deque<double>, 10>;
 
   //! A server in the simulation, processing a single customer at a time.
   struct Server {
-    explicit Server()
-        : theBusy(false)
-        , theLastArrival(0) {
-    }
+    explicit Server() : theBusy(false), theLastArrival(0) {}
 
-    bool   theBusy;
+    bool theBusy;
     double theLastArrival;
   };
 
@@ -39,33 +64,26 @@ class MMC final
 
     //! Create an arrival event.
     explicit Event(const double aExpireTime) noexcept
-        : theType(ARRIVAL)
-        , theServerIndex(0)
-        , theExpireTime(aExpireTime) {
-    }
+        : theType(ARRIVAL), theServerIndex(0), theExpireTime(aExpireTime) {}
 
     //! Create a departure event.
     explicit Event(const double aExpireTime, const size_t aServerIndex) noexcept
-        : theType(DEPARTURE)
-        , theServerIndex(aServerIndex)
-        , theExpireTime(aExpireTime) {
-    }
+        : theType(DEPARTURE), theServerIndex(aServerIndex),
+          theExpireTime(aExpireTime) {}
 
-    bool operator<(const Event& aOther) const noexcept {
+    bool operator<(const Event &aOther) const noexcept {
       return theExpireTime > aOther.theExpireTime;
     }
 
-    Type   theType;
+    Type theType;
     size_t theServerIndex; // only if DEPARTURE
     double theExpireTime;
   };
 
- public:
+public:
   //! Initialize a Marvov queue M/M/c model.
-  explicit MMC(const double       lambda_,
-               const double       mu_,
-               const unsigned int c_,
-               const size_t       aSeedSeq);
+  explicit MMC(const double lambda_, const double mu_, const unsigned int c_,
+               const size_t aSeedSeq);
 
   //! \return the steady-state average response time.
   double averageResponseTime() const;
@@ -74,53 +92,47 @@ class MMC final
   void run(const double aDuration);
 
   //! \return the average delay measured so far.
-  double averageDelay() const {
-    return theDelayStat.mean();
-  }
+  double averageDelay() const { return theDelayStat.mean(); }
 
   //! Enable statistics collection.
-  void statEnable() {
-    theStatEnabled = true;
-  }
+  void statEnable() { theStatEnabled = true; }
 
   //! Sort the delays per load value per increasing delay.
   void sortDelays();
 
   //! \return the delays per load value.
-  const DelayArray& delays() const {
-    return theDelays;
-  }
+  const DelayArray &delays() const { return theDelays; }
 
- private:
+private:
   //! A new customer arrived.
   void arrival();
 
   //! A customer departed from a given server.
   void departure(const size_t aServerIndex);
 
- private:
+private:
   // const ctor arguments
-  const double       lambda;
-  const double       mu;
+  const double lambda;
+  const double mu;
   const unsigned int c;
-  const double       theLoadPeriod;
+  const double theLoadPeriod;
 
   // derived
-  const double                                       rho;
-  std::vector<std::unique_ptr<std::seed_seq>>        theSeedSeqs;
-  std::vector<std::mt19937>                          theGenerators;
+  const double rho;
+  std::vector<std::unique_ptr<std::seed_seq>> theSeedSeqs;
+  std::vector<std::mt19937> theGenerators;
   std::vector<std::exponential_distribution<double>> theExpos;
 
   // internal state
-  bool                       theStatEnabled;
-  double                     theClock;
-  std::vector<Server>        theServers;
-  size_t                     theBusyServers;
-  std::list<double>          theQueue;
+  bool theStatEnabled;
+  double theClock;
+  std::vector<Server> theServers;
+  size_t theBusyServers;
+  std::list<double> theQueue;
   std::priority_queue<Event> theEvents;
-  support::SummaryStat       theDelayStat;
-  DelayArray                 theDelays;
-  LoadList                   theLoads;
+  support::SummaryStat theDelayStat;
+  DelayArray theDelays;
+  LoadList theLoads;
 };
 
 } // namespace markov
