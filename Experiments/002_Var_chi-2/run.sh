84eea0d9b0840c8a6a522fbff2436c4e516d4b43
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2019-09-27 17:22:02 +0200

    Add Experiment 002_Var_chi-2

---
+++ Experiments/002_Var_chi-2/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+runs=100
+clients="5 6 7 8 9 10 11"
+chi_values="0.001 0.002 0.005 0.01 0.02 0.05 0.1 0.2"
+
+for c in $clients ; do
+  for chi in $chi_values ; do
+    echo "$c clients, chi $chi"
+
+    python ../../Serverless/serverless.py \
+      --clients $c \
+      --servers 6
+      --chi $chi \
+      --mu_min 8 --mu_max 12 \
+      --load_min 1 --load_max 3 \
+      --threads 1 \
+      --runs $runs \
+      --output out.chi=$chi.c=$c.dat
+  done
+done
