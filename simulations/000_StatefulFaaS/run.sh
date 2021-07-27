c6e0139beaf7c908c55b96cbcb831fe462ceaf63
Claudio Cicconetti <ccicconetti@mbigroup.it>
2021-07-27 15:11:39 +0200

    Add script to generate artifacts with sfm-latency

---
+++ simulations/000_StatefulFaaS/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir results 2> /dev/null
+
+for (( clients = 40 ; clients <= 120 ; clients += 10 )) ; do
+  ./sfm-latency \
+    --arrival-rate=0.075 \
+    --clients=$clients \
+    --containers=40 \
+    --service-time-full=1 \
+    --service-time-less=0.333 \
+    --output results/out-$clients.dat
+done
