451bd739acbafcf10ccdc13e042747e791d056bf
Claudio Cicconetti <ccicconetti@mbigroup.it>
2021-07-28 19:34:31 +0200

    Update simulation 000 script

--- simulations/000_StatefulFaaS/run-provisioning.sh
+++ simulations/000_StatefulFaaS/run-provisioning.sh
@@ -2,14 +2,20 @@
 
 mkdir results 2> /dev/null
 
-rm -f results/provisioning.dat 2> /dev/null
-for (( clients = 5 ; clients <= 100 ; clients += 1 )) ; do
-  ret=$(./sfm-provisioning \
-    --arrival-rate=0.075 \
-    --clients=$clients \
-    --service-time-full=1 \
-    --service-time-less=3 \
-    --q-full 20 \
-    --q-less 80)
-  echo "$clients $ret" >> results/provisioning.dat
+qf_values="20 30 40"
+
+for qf in $qf_values ; do
+  rm -f results/provisioning-$qf.dat 2> /dev/null
+  ql=$((100-qf))
+  for (( clients = 5 ; clients <= 100 ; clients += 1 )) ; do
+    echo "qf $qf, ql $ql, clients $clients"
+    ret=$(./sfm-provisioning \
+      --arrival-rate=0.075 \
+      --clients=$clients \
+      --service-time-full=1 \
+      --service-time-less=3 \
+      --q-full $qf \
+      --q-less $ql)
+    echo "$clients $ret" >> results/provisioning-$qf.dat
+  done
 done
